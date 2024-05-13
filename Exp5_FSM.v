a8bf09023af786bccc31ccec892ce9e4e93005ec
harikaran814 <164861651+harikaran814@users.noreply.github.com>
2024-05-13 15:16:20 +0530

    Update Exp5_FSM.v

--- Exp5_FSM.v
+++ Exp5_FSM.v
@@ -1,39 +1,3 @@
-# VLSI-LAB-EXP-5
-SIMULATION AND IMPLEMENTATION OF FINITE STATE MACHINE
-
-AIM: To simulate and synthesis finite state machine using vivado2023.3.
-
-**APPARATUS REQUIRED:**
-
-vivado 2023.3
-
-**PROCEDURE:**
-
-
-1.Open Vivado: Launch Xilinx Vivado software on your computer.
-
-2.Create a New Project: Click on "Create Project" from the welcome page or navigate through "File" > "Project" > "New".
-
-3.Project Settings: Follow the prompts to set up your project. Specify the project name, location, and select RTL project type.
-
-4.Add Design Files: Add your Verilog design files to the project. You can do this by right-clicking on "Design Sources" in the Sources window, then selecting "Add Sources". Choose your Verilog files from the file browser.
-
-5.Specify Simulation Settings: Go to "Simulation" > "Simulation Settings". Choose your simulation language (Verilog in this case) and simulation tool (Vivado Simulator).
-
-6.Run Simulation: Go to "Flow" > "Run Simulation" > "Run Behavioral Simulation". This will launch the Vivado Simulator and compile your design for simulation.
-
-7.Set Simulation Time: In the Vivado Simulator window, set the simulation time if it's not set automatically. This determines how long the simulation will run.
-
-8.Run Simulation: Start the simulation by clicking on the "Run" button in the simulation window.
-
-9.View Results: After the simulation completes, you can view waveforms, debug signals, and analyze the behavior of your design.
-Logic Diagram :
-
-![image](https://github.com/navaneethans/VLSI-LAB-EXP-5/assets/6987778/34ec5d63-2b3b-4511-81ef-99f4572d5869)
-
-
-VERILOG CODE:
-~~~
 module Sequence_Detector_Moore(clock,reset,sequence_in,detector_out);
 input clock, reset, sequence_in; 
 output reg detector_out; 
@@ -90,12 +54,3 @@ begin
  endcase
 end 
 endmodule
-~~~
-
-OUTPUT:
-![fsm11](https://github.com/CalebSamraj14/VLSI-LAB-EXP-5/assets/163808923/89457bfc-2a68-47e9-8a46-f26c4fbcb487)
-
-
-RESULT:
-
-Thus the simulate and synthesis finite state machine using vivado2023.3 is verified
